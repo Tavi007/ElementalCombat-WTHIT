7c6253317d96d290e2271b9c3b04b6a1a913db36
Tavi007 <Tavi007@users.noreply.github.com>
2024-06-15 20:07:06 +0200

    now using the new api methods

--- src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
+++ src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
@@ -1,24 +1,16 @@
 package Tavi007.ElementalCombatWTHIT;
 
 import java.awt.Dimension;
-import java.util.ArrayList;
-import java.util.List;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 
-import Tavi007.ElementalCombat.capabilities.attack.AttackData;
-import Tavi007.ElementalCombat.capabilities.defense.DefenseData;
-import Tavi007.ElementalCombat.config.ClientConfig;
-import Tavi007.ElementalCombat.util.AttackDataHelper;
-import Tavi007.ElementalCombat.util.DefenseDataHelper;
-import Tavi007.ElementalCombat.util.RenderHelper;
+import Tavi007.ElementalCombat.api.ElementalCombatDataRenderAPI;
 import mcp.mobius.waila.api.ICommonAccessor;
 import mcp.mobius.waila.api.ITooltipRenderer;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.text.ITextComponent;
 
 public class WailaTooltipRenderer implements ITooltipRenderer {
 
@@ -26,68 +18,27 @@ public class WailaTooltipRenderer implements ITooltipRenderer {
     public Dimension getSize(CompoundNBT data, ICommonAccessor accessor) {
         if (accessor.getEntity() != null && accessor.getEntity() instanceof LivingEntity) {
             LivingEntity living = (LivingEntity) accessor.getEntity();
-            int height = RenderHelper.maxLineHeight;
-            DefenseData defenseData = DefenseDataHelper.get(living);
-            if (!defenseData.isEmpty()) {
-                height += RenderHelper.maxLineHeight;
-                if (ClientConfig.isDoubleRowDefenseHWYLA() && !defenseData.getElementFactor().isEmpty() && !defenseData.getStyleFactor().isEmpty()) {
-                    height += RenderHelper.maxLineHeight;
-                }
-            }
-            return new Dimension(RenderHelper.maxLineWidth, height);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(true, living);
         } else if (accessor.getStack() != null) {
             ItemStack stack = accessor.getStack();
-            int height = 0;
-            if (!AttackDataHelper.get(stack).isDefault()) {
-                height += RenderHelper.maxLineHeight;
-            }
-            DefenseData defenseData = DefenseDataHelper.get(stack);
-            if (!defenseData.isEmpty()) {
-                height += RenderHelper.maxLineHeight;
-                if (ClientConfig.isDoubleRowDefenseHWYLA() && !defenseData.getElementFactor().isEmpty() && !defenseData.getStyleFactor().isEmpty()) {
-                    height += RenderHelper.maxLineHeight;
-                }
-
-            }
-            return new Dimension(RenderHelper.maxLineWidth, height);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(true, stack);
         }
         return new Dimension();
     }
 
     @Override
     public void draw(CompoundNBT data, ICommonAccessor accessor, int x, int y) {
-        AttackData attackData;
-        DefenseData defenseData;
-        // check entity
         Entity entity = accessor.getEntity();
-        ItemStack stack = accessor.getStack();
         if (entity != null && entity instanceof LivingEntity) {
-            LivingEntity livingEntity = (LivingEntity) entity;
-            attackData = AttackDataHelper.get(livingEntity);
-            defenseData = DefenseDataHelper.get(livingEntity);
-        } else if (stack != null) {
-            attackData = AttackDataHelper.get(stack);
-            defenseData = DefenseDataHelper.get(stack);
-        } else {
-            attackData = null;
-            defenseData = null;
+            ElementalCombatDataRenderAPI.renderTextAndIcons(new MatrixStack(), x, y, true, (LivingEntity) entity);
+            return;
         }
 
-        // rendering starts here
-        MatrixStack matrixStack = new MatrixStack();
-        List<ITextComponent> tooltip = new ArrayList<ITextComponent>();
-        RenderHelper.addTooltip(tooltip, ClientConfig.isDoubleRowDefenseHWYLA(), attackData, defenseData);
-        RenderHelper.renderTooltip(tooltip, matrixStack, x, y);
-
-        if (attackData != null) {
-            RenderHelper.renderAttackIcons(attackData, matrixStack, x, y);
-            y += RenderHelper.maxLineHeight;
-
+        ItemStack stack = accessor.getStack();
+        if (stack != null) {
+            ElementalCombatDataRenderAPI.renderTextAndIcons(new MatrixStack(), x, y, true, stack);
+            return;
         }
-        if (defenseData != null && !defenseData.isEmpty()) {
-            RenderHelper.renderDefenseIcons(defenseData, ClientConfig.isDoubleRowDefenseHWYLA(), matrixStack, x, y);
-        }
-
     }
 
 }
