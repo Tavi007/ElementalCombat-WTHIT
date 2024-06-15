6092b484b991c0379c74573927e8a050f7b2a96d
Tavi007 <Tavi007@users.noreply.github.com>
2024-06-15 14:22:39 +0200

    init commit
    moving wthit support to its own mod

---
+++ src/main/java/Tavi007/ElementalCombatWTHIT/CombatPropertiesWailaPlugin.java
@@ -0,0 +1,21 @@
+package Tavi007.ElementalCombatWTHIT;
+
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.TooltipPosition;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.ResourceLocation;
+
+@WailaPlugin(ElementalCombatWTHIT.MOD_ID)
+public class CombatPropertiesWailaPlugin implements IWailaPlugin {
+
+    static final ResourceLocation COMBAT_PROPERTIES = new ResourceLocation(ElementalCombatWTHIT.MOD_ID, "combat_properties");
+
+    @Override
+    public void register(IRegistrar registrar) {
+        registrar.addComponent(HUDHandlerEntities.INSTANCE, TooltipPosition.BODY, LivingEntity.class);
+        registrar.addRenderer(COMBAT_PROPERTIES, new WailaTooltipRenderer());
+        ElementalCombatWTHIT.LOGGER.info("Waila Plugin registered.");
+    }
+}
---
+++ src/main/java/Tavi007/ElementalCombatWTHIT/ElementalCombatWTHIT.java
@@ -0,0 +1,20 @@
+package Tavi007.ElementalCombatWTHIT;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("elementalcombat_wthit")
+public class ElementalCombatWTHIT {
+
+    public static ElementalCombatWTHIT instance;
+    public static final String MOD_ID = "elementalcombat_wthit";
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+
+    public ElementalCombatWTHIT() {
+        instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
