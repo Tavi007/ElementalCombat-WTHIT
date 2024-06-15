4b120e784d977119bc35216bcc66a921a5160627
Tavi007 <Tavi007@users.noreply.github.com>
2024-06-15 23:11:07 +0200

    now also uses client config

--- src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
+++ src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
@@ -18,10 +18,10 @@ public class WailaTooltipRenderer implements ITooltipRenderer {
     public Dimension getSize(CompoundTag data, ICommonAccessor accessor) {
         if (accessor.getEntity() != null && accessor.getEntity() instanceof LivingEntity) {
             LivingEntity living = (LivingEntity) accessor.getEntity();
-            return ElementalCombatDataRenderAPI.getTooltipDimension(true, living);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(ClientConfig.isDefenseDoubleRow(), living);
         } else if (accessor.getStack() != null) {
             ItemStack stack = accessor.getStack();
-            return ElementalCombatDataRenderAPI.getTooltipDimension(true, stack);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(ClientConfig.isDefenseDoubleRow(), stack);
         }
         return new Dimension();
     }
