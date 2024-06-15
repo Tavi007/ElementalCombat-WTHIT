235547890036c3b405005432be8f05eaaa41672a
Tavi007 <Tavi007@users.noreply.github.com>
2024-06-15 23:10:47 +0200

    now also uses the client config

--- src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
+++ src/main/java/Tavi007/ElementalCombatWTHIT/WailaTooltipRenderer.java
@@ -18,10 +18,10 @@ public class WailaTooltipRenderer implements ITooltipRenderer {
     public Dimension getSize(CompoundTag data, ICommonAccessor accessor) {
         if (accessor.getEntity() != null && accessor.getEntity() instanceof LivingEntity) {
             LivingEntity living = (LivingEntity) accessor.getEntity();
-            return ElementalCombatDataRenderAPI.getTooltipDimension(true, living);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(ClientConfig.isDefenseDoubleRow(), living);
         } else if (accessor.getStack() != null) {
             ItemStack stack = accessor.getStack();
-            return ElementalCombatDataRenderAPI.getTooltipDimension(true, stack);
+            return ElementalCombatDataRenderAPI.getTooltipDimension(ClientConfig.isDefenseDoubleRow(), stack);
         }
         return new Dimension();
     }
@@ -30,13 +30,13 @@ public class WailaTooltipRenderer implements ITooltipRenderer {
     public void draw(PoseStack poseStack, CompoundTag data, ICommonAccessor accessor, int x, int y) {
         Entity entity = accessor.getEntity();
         if (entity != null && entity instanceof LivingEntity) {
-            ElementalCombatDataRenderAPI.renderTextAndIcons(poseStack, x, y, true, (LivingEntity) entity);
+            ElementalCombatDataRenderAPI.renderTextAndIcons(poseStack, x, y, ClientConfig.isDefenseDoubleRow(), (LivingEntity) entity);
             return;
         }
 
         ItemStack stack = accessor.getStack();
         if (stack != null) {
-            ElementalCombatDataRenderAPI.renderTextAndIcons(poseStack, x, y, true, stack);
+            ElementalCombatDataRenderAPI.renderTextAndIcons(poseStack, x, y, ClientConfig.isDefenseDoubleRow(), stack);
             return;
         }
     }
